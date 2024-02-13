3ca2589c01af1b74fa7456f45b767e0560a9ddcb
Alain Chevanier <alain.chevanier@ciencias.unam.mx>
2024-02-12 20:44:46 -0600

    Cleaning some unneeded code from simple thread

--- src/main/java/unam/ciencias/computoconcurrente/threadobjects/SimpleThreads.java
+++ src/main/java/unam/ciencias/computoconcurrente/threadobjects/SimpleThreads.java
@@ -11,46 +11,35 @@ public class SimpleThreads {
     // thread (default one hour).
     long patience = 1000 * 10; // 60 * 60;
 
-    // If command line argument
-    // present, gives patience
-    // in seconds.
-    if (args.length > 0) {
-      try {
-        patience = Long.parseLong(args[0]) * 1000;
-      } catch (NumberFormatException e) {
-        System.err.println("Argument must be an integer.");
-        System.exit(1);
-      }
-    }
 
     threadMessage("Starting MessageLoop thread");
     long startTime = System.currentTimeMillis();
 
-    Thread t = new Thread(new MessageLoop());
-    t.start();
+    Thread childThread = new Thread(new MessageLoop());
+    childThread.start();
 
     threadMessage("Waiting for MessageLoop thread to finish");
     // loop until MessageLoop
     // thread exits
-    while (t.isAlive()) {
+    while (childThread.isAlive()) {
       threadMessage("Still waiting...");
       // Wait maximum of 1 second
       // for MessageLoop thread
       // to finish.
-      t.join(1000);
+      childThread.join(1000);
 
-      if (isDoneWaiting(patience, startTime, t)) {
+      if (amIDoneWaiting(patience, startTime, childThread)) {
         threadMessage("Tired of waiting!");
-        t.interrupt();
+        childThread.interrupt();
         // Shouldn't be long now
         // -- wait indefinitely
-        t.join();
+        childThread.join();
       }
     }
     threadMessage("Finally!");
   }
 
-  private static boolean isDoneWaiting(long patience, long startTime, Thread t) {
+  private static boolean amIDoneWaiting(long patience, long startTime, Thread t) {
     long elapsedTime = System.currentTimeMillis() - startTime;
     return (elapsedTime > patience) && t.isAlive();
   }
@@ -71,8 +60,8 @@ public class SimpleThreads {
       };
       // wait using sleep
       try {
-        for (int i = 0; i < importantInfo.length; i++) {
-          // Pause for 4 seconds
+        for (String string : importantInfo) {
+          // Pause for ~4 seconds
           Thread.sleep(4000);
           // Print a message
           threadMessage(importantInfo[i]);
@@ -80,8 +69,6 @@ public class SimpleThreads {
       } catch (InterruptedException e) {
         threadMessage("I wasn't done!");
       }
-
-      // espera utilizando un ciclo de computo muy largo
      }
   }
 }
