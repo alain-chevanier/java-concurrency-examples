b132379600b4d3e7b691b2b2efb44517e9ae1d14
Alain Chevanier <alain.chevanier@nubank.com.br>
2024-08-12 23:56:15 -0600

    refactor find minimum

--- src/main/java/unam/ciencias/computoconcurrente/threadobjects/MultiThreadedMatrixFindMinimum.java
+++ src/main/java/unam/ciencias/computoconcurrente/threadobjects/MultiThreadedMatrixFindMinimum.java
@@ -1,6 +1,7 @@
 package unam.ciencias.computoconcurrente.threadobjects;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Objects;
@@ -17,47 +18,61 @@ public class MultiThreadedMatrixFindMinimum extends MultiThreadedOperation
   }
 
   @Override
-  public <N extends Comparable<N>> N findMinimum(Matrix<N> matrix) throws InterruptedException {
-    List<N> minimumsList = new ArrayList<>(this.threads);
+  public <N extends Comparable<N>> N findMinimum(Matrix<N> matrix)
+    throws InterruptedException {
+    List<N> minimums = createMinimumsList();
+    List<Thread> threadList = createThreadList(matrix, minimums);
+    this.runThreads(threadList);
+    this.waitThreads(threadList);
+    return minimums.stream().min(N::compareTo)
+      .orElseThrow(NoSuchElementException::new);
+  }
+
+  private <N extends Comparable<N>> List<N>
+    createMinimumsList() {
+    List<N> minimums = new ArrayList<>(this.threads);
+    for (int i = 0; i < this.threads; i++) {
+      minimums.add(null);
+    }
+    return minimums;
+  }
+
+  private <N extends Comparable<N>> List<Thread>
+    createThreadList(Matrix<N> matrix, List<N> minimums) {
     List<Thread> threadList = new ArrayList<>(this.threads);
     for (int i = 0; i < this.threads; i++) {
-      minimumsList.add(null);
-    }
-    for (int i = 1; i < this.threads; i++) {
       int threadId = i;
-      threadList.add(new Thread(() -> taskFindMinimum(threadId, minimumsList, matrix)));
+      threadList.add(new Thread(() ->
+                                taskFindMinimum(threadId, matrix, minimums)));
     }
-    this.runThreads(threadList);
-    taskFindMinimum(0, minimumsList, matrix);
-    this.waitThreads(threadList);
-    return minimumsList.stream().min(N::compareTo).orElseThrow(NoSuchElementException::new);
+    return threadList;
   }
 
   private <N extends Comparable<N>> void taskFindMinimum(
-      int threadId, List<N> minimumsList, Matrix<N> matrix) {
-    N min = null;
+      int threadId, Matrix<N> matrix, List<N> minimums) {
     int chunkSize = matrix.getRows() / this.threads;
-    int initRow =
-        threadId * chunkSize + (threadId == this.threads - 1 ? matrix.getRows() % chunkSize : 0);
-    for (int i = initRow; i < initRow + chunkSize; i++) {
+    int initRow = threadId * chunkSize;
+    // in case matrix.getRows() % chunkSize != 0,
+    // the last thread process the remaning rows
+    int remainingWork = threadId == this.threads - 1 ?
+                          matrix.getRows() % chunkSize : 0;
+    int lastRow = initRow + chunkSize + remainingWork;
+    N min = null;
+    for (int i = initRow; i < lastRow; i++) {
       N rowMin = this.findMinimumInRow(matrix.getRow(i));
-      if (min == null) {
+      if (Objects.isNull(min) ||
+          rowMin.compareTo(min) < 0) {
         min = rowMin;
-      } else {
-        min = rowMin.compareTo(min) < 0 ? rowMin : min;
       }
     }
-    minimumsList.set(threadId, min);
+    minimums.set(threadId, min);
   }
 
   private <N extends Comparable<N>> N findMinimumInRow(List<N> values) {
     N minimum = null;
     for (N value : values) {
-      if (Objects.isNull(minimum)) {
-        minimum = value;
-        continue;
-      }
-      if (value.compareTo(minimum) < 0) {
+      if (Objects.isNull(minimum) ||
+          value.compareTo(minimum) < 0) {
         minimum = value;
       }
     }
